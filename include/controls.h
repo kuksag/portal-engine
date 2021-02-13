7cca59f137aecb6193081acdaeac47a9b5fc0028
kuksag <georgiy.kuksa@gmail.com>
2021-02-13 16:47:02 +0300

    added controls

--- include/controls.h
+++ include/controls.h
@@ -1,8 +1,44 @@
 #ifndef PORTAL_ENGINE_CONTROLS_H
 #define PORTAL_ENGINE_CONTROLS_H
 
-struct Controls {
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
+struct Game;
+
+struct Controller {
+private:
+    // TODO: use unique_ptr?
+    Game *game;
+    float last_time_point;
+
+public:
+    explicit Controller(Game *game_)
+        : game(game_), last_time_point() {
+        update_time();
+    }
+
+    void scroll_callback(double y_delta);
+    void cursor_position_callback(double x_delta, double y_delta);
+    void key_callback(int key, int action);
+
+    void update_time();
+
+    // TODO:
+    //    void mouse_button_callback(int button, int action, int mods);
+    //    void window_focus_callback(int focused);
 };
+
+void scroll_callback(GLFWwindow *window, double x_delta, double y_delta);
+void cursor_position_callback(GLFWwindow *window, double x_delta,
+                                     double y_delta);
+void key_callback(GLFWwindow *window, int key, int scancode, int action,
+                         int mods);
+
+// TODO:
+//  static void mouse_button_callback(GLFWwindow *window, int button, int
+//  action,
+//                                  int mods);
+//  static void window_focus_callback(GLFWwindow *window, int focused);
 
 #endif    // PORTAL_ENGINE_CONTROLS_H
--- src/controls.cpp
+++ src/controls.cpp
@@ -1,1 +1,73 @@
 #include "controls.h"
+
+#include <algorithm>
+
+#include "game.h"
+#include "settings.h"
+using namespace Settings::Controls;
+
+void Controller::scroll_callback(double y_delta) {
+    game->camera->process_mouse_scroll(static_cast<float>(y_delta));
+}
+
+void Controller::cursor_position_callback(double x_delta, double y_delta) {
+    game->camera->process_mouse_move(static_cast<float>(x_delta),
+                                     static_cast<float>(y_delta));
+}
+
+void Controller::key_callback(int key, int action) {
+    auto in_set = [&](const std::vector<int> &SET_OF_KEYS) {
+        return std::find(SET_OF_KEYS.begin(), SET_OF_KEYS.end(), key) !=
+               SET_OF_KEYS.end();
+    };
+
+    auto current_time = static_cast<float>(glfwGetTime());
+    auto time_delta = current_time - last_time_point;
+
+    if (action == GLFW_PRESS || action == GLFW_REPEAT) {
+        if (in_set(KEY_FORWARD)) game->camera->move_forward(time_delta);
+        if (in_set(KEY_RIGHT)) game->camera->move_right(time_delta);
+        if (in_set(KEY_BACKWARD)) game->camera->move_backward(time_delta);
+        if (in_set(KEY_LEFT)) game->camera->move_left(time_delta);
+    }
+
+    // TODO: add `full_screen` key
+
+    last_time_point = current_time;
+}
+
+void Controller::update_time() {
+    last_time_point = static_cast<float>(glfwGetTime());
+}
+
+void scroll_callback(GLFWwindow *window, double x_delta, double y_delta) {
+    auto *controller =
+        reinterpret_cast<Controller *>(glfwGetWindowUserPointer(window));
+    if (controller) {
+        controller->scroll_callback(y_delta);
+    } else {
+        assert(false);
+    }
+}
+
+void cursor_position_callback(GLFWwindow *window, double x_delta,
+                              double y_delta) {
+    auto *controller =
+        reinterpret_cast<Controller *>(glfwGetWindowUserPointer(window));
+    if (controller) {
+        controller->cursor_position_callback(x_delta, y_delta);
+    } else {
+        assert(false);
+    }
+}
+
+void key_callback(GLFWwindow *window, int key, int scancode, int action,
+                  int mods) {
+    auto *controller =
+        reinterpret_cast<Controller *>(glfwGetWindowUserPointer(window));
+    if (controller) {
+        controller->key_callback(key, action);
+    } else {
+        assert(false);
+    }
+}
