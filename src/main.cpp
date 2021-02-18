4154f1aed328769c660ff82b2235f995649feecc
kuksag <georgiy.kuksa@gmail.com>
2021-02-18 23:30:52 +0300

    attached light

--- src/main.cpp
+++ src/main.cpp
@@ -118,48 +118,35 @@ int main() {
     //--------------------------------------------------------------------------
     // Освещаемый куб
     static const GLfloat light_cube_vertexes[] = {
-        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-        0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-        0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-        0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f,  0.0f,  -1.0f, 0.5f,  -0.5f, -0.5f,
+        0.0f,  0.0f,  -1.0f, 0.5f,  0.5f,  -0.5f, 0.0f,  0.0f,  -1.0f,
+        0.5f,  0.5f,  -0.5f, 0.0f,  0.0f,  -1.0f, -0.5f, 0.5f,  -0.5f,
+        0.0f,  0.0f,  -1.0f, -0.5f, -0.5f, -0.5f, 0.0f,  0.0f,  -1.0f,
 
-        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-        0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-        0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-        0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+        -0.5f, -0.5f, 0.5f,  0.0f,  0.0f,  1.0f,  0.5f,  -0.5f, 0.5f,
+        0.0f,  0.0f,  1.0f,  0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
+        0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,  -0.5f, 0.5f,  0.5f,
+        0.0f,  0.0f,  1.0f,  -0.5f, -0.5f, 0.5f,  0.0f,  0.0f,  1.0f,
 
-        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, 0.5f,  0.5f,  -1.0f, 0.0f,  0.0f,  -0.5f, 0.5f,  -0.5f,
+        -1.0f, 0.0f,  0.0f,  -0.5f, -0.5f, -0.5f, -1.0f, 0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f, 0.0f,  0.0f,  -0.5f, -0.5f, 0.5f,
+        -1.0f, 0.0f,  0.0f,  -0.5f, 0.5f,  0.5f,  -1.0f, 0.0f,  0.0f,
 
-        0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-        0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-        0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-        0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-        0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-        0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,  0.5f,  0.5f,  -0.5f,
+        1.0f,  0.0f,  0.0f,  0.5f,  -0.5f, -0.5f, 1.0f,  0.0f,  0.0f,
+        0.5f,  -0.5f, -0.5f, 1.0f,  0.0f,  0.0f,  0.5f,  -0.5f, 0.5f,
+        1.0f,  0.0f,  0.0f,  0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
 
-        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-        0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-        0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-        0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f, 0.0f,  -1.0f, 0.0f,  0.5f,  -0.5f, -0.5f,
+        0.0f,  -1.0f, 0.0f,  0.5f,  -0.5f, 0.5f,  0.0f,  -1.0f, 0.0f,
+        0.5f,  -0.5f, 0.5f,  0.0f,  -1.0f, 0.0f,  -0.5f, -0.5f, 0.5f,
+        0.0f,  -1.0f, 0.0f,  -0.5f, -0.5f, -0.5f, 0.0f,  -1.0f, 0.0f,
 
-        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
-        0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
-        0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-        0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
-    };
+        -0.5f, 0.5f,  -0.5f, 0.0f,  1.0f,  0.0f,  0.5f,  0.5f,  -0.5f,
+        0.0f,  1.0f,  0.0f,  0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+        0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,  -0.5f, 0.5f,  0.5f,
+        0.0f,  1.0f,  0.0f,  -0.5f, 0.5f,  -0.5f, 0.0f,  1.0f,  0.0f};
 
     GLuint light_vertex_buffer, light_vertex_array_id;
     glGenVertexArrays(1, &light_vertex_array_id);
@@ -170,12 +157,12 @@ int main() {
     glBindBuffer(GL_ARRAY_BUFFER, light_vertex_buffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(light_cube_vertexes),
                  light_cube_vertexes, GL_STATIC_DRAW);
-    //vertex coords
+    // vertex coords
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat),
                           (GLvoid *)0);
     glEnableVertexAttribArray(0);
 
-    //normal
+    // normal
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat),
                           (GLvoid *)(3 * sizeof(GLfloat)));
     glEnableVertexAttribArray(1);
@@ -185,8 +172,7 @@ int main() {
     ShaderProgram light_shader("light.vertex", "light.fragment");
 
     // -------------------------------------------------------------------------
-    GLfloat fancy_triangle[] = {// positions         // colors
-                                1.0f, 0.0f, 0.0,  1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+    GLfloat fancy_triangle[] = {1.0f, 0.0f, 0.0,  1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
                                 0.0,  0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0,  0.5f,
                                 0.5f, 0.5f, 0.0f, 0.0f, 1.0,  0.0f, 0.0f, 1.0f};
 
@@ -257,14 +243,16 @@ int main() {
         // ---------------------------------------------------------------------
         float timeValue = glfwGetTime();
         fancy_shader.use();
-        glUniform3f(fancy_shader.get_uniform_id("fancy_color"), sin(timeValue),
-                    0.2, cos(timeValue));
+        glm::vec3 current_fancy_color = {sin(timeValue), 0.2, cos(timeValue)};
+        glUniform3f(fancy_shader.get_uniform_id("fancy_color"),
+                    current_fancy_color[0], current_fancy_color[1],
+                    current_fancy_color[2]);
 
         glm::mat4 translation_matrix(1.0f), scale_matrix(1.0f);
         scale_matrix =
-            glm::scale(scale_matrix, glm::vec3(sin(glfwGetTime() / 8) * 6.0f,
-                                               sin(glfwGetTime() / 8) * 6.0f,
-                                               sin(glfwGetTime() / 8) * 6.0f));
+            glm::scale(scale_matrix, glm::vec3(sin(glfwGetTime() / 8) * 2.0f,
+                                               sin(glfwGetTime() / 8) * 2.0f,
+                                               sin(glfwGetTime() / 8) * 2.0f));
         translation_matrix = glm::translate(
             translation_matrix, glm::vec3(sin(glfwGetTime() / 4) * 0.8f,
                                           cos(glfwGetTime() / 32) * 0.5f,
@@ -273,6 +261,16 @@ int main() {
         glm::mat4 rotation_matrix(1.0f);
         rotation_matrix = glm::rotate(rotation_matrix, (float)glfwGetTime(),
                                       glm::vec3(0.4f, 0.8f, -0.1f));
+
+        glm::mat4 current_center_m4 = scale_matrix * rotation_matrix *
+                                      translation_matrix *
+                                      glm::mat4({{1.0f, 1.0f, 1.0f, 1.0f},
+                                                 {1.0f, 1.0f, 1.0f, 1.0f},
+                                                 {1.0f, 1.0f, 1.0f, 1.0f},
+                                                 {1.0f, 1.0f, 1.0f, 1.0f}});
+        glm::vec3 current_center_v3 = {current_center_m4[0][0],
+                                       current_center_m4[0][1],
+                                       current_center_m4[0][2]};
 
         MVP = camera.get_projection_matrix() * camera.get_view_matrix() *
               scale_matrix * rotation_matrix * translation_matrix;
@@ -283,16 +281,28 @@ int main() {
         glBindVertexArray(0);
         // ---------------------------------------------------------------------
         light_shader.use();
-        glm::mat4 MVP_trans = camera.get_projection_matrix() *
-                              camera.get_view_matrix() * glm::mat4(1.0f);
+        glm::mat4 MVP_trans =
+            camera.get_projection_matrix() * camera.get_view_matrix() *
+            glm::translate(glm::mat4(1.0f), glm::vec3(3.0f, 3.0f, 3.0f));
         glUniformMatrix4fv(light_shader.get_uniform_id("MVP"), 1, GL_FALSE,
                            &MVP_trans[0][0]);
-        glUniform3f(light_shader.get_uniform_id("light_color"), 1.0f, 1.0f, 1.0f);
-        glUniform3f(light_shader.get_uniform_id("light_pos"), -3.0f, -1.0f, -3.0f);
-        glUniform3f(light_shader.get_uniform_id("trans"), 3.0f, 3.0f, 3.0f);
+        glUniform3f(light_shader.get_uniform_id("light_color"),
+                    current_fancy_color[0], current_fancy_color[1],
+                    current_fancy_color[2]);
+        glUniform3f(light_shader.get_uniform_id("light_pos"),
+                    current_center_v3[0], current_center_v3[1],
+                    current_center_v3[2]);
         glBindVertexArray(light_vertex_array_id);
         glDrawArrays(GL_TRIANGLES, 0, 36);
         glBindVertexArray(0);
+        //----------------------------------------------------------------------
+        
+        float distance = 0;
+        for (int i = 0; i < 3; i++) {
+            distance += (current_center_v3[i] - camera.get_position()[i]) * (current_center_v3[i] - camera.get_position()[i]);
+        }
+        std::cerr << sqrt(distance) << '\n';
+
         //----------------------------------------------------------------------
 
         glfwSwapBuffers(window);
