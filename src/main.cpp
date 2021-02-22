07e4f74feace172b10e476472b9529acc05247de
kuksag <georgiy.kuksa@gmail.com>
2021-02-22 21:49:09 +0300

    doesnt work yet; need to merge with adding-light

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "controls.h"
 #include "settings.h"
 #include "shader.h"
+#include "SceneObjects/cube.h"
 #include "SceneObjects/triangle.h"
 
 #include "SceneObjects/pyramid.h"
@@ -53,79 +54,10 @@ int main() {
     GLFWwindow *window = nullptr;
     window_initialise(window);
     // -------------------------------------------------------------------------
-    ShaderProgram temp_shader("shaders/temp.vertex", "shaders/temp.fragment");
-
     Triangle textured_triangle;
-    std::cout << glGetError() << std::endl;
-    //textured_triangle.translate(glm::vec3(-0.5f, -0.5f, -0.5f));
     //--------------------------------------------------------------------------
-    // Освещаемый куб
-    static const GLfloat light_cube_vertexes[] = {
-        -0.5f, -0.5f, -0.5f,    0.0f,  0.0f,  -1.0f,
-        0.5f,  -0.5f, -0.5f,    0.0f,  0.0f,  -1.0f,
-        0.5f,  0.5f,  -0.5f,    0.0f,  0.0f,  -1.0f,
-        0.5f,  0.5f,  -0.5f,    0.0f,  0.0f,  -1.0f,
-        -0.5f, 0.5f,  -0.5f,    0.0f,  0.0f,  -1.0f,
-        -0.5f, -0.5f, -0.5f,    0.0f,  0.0f,  -1.0f,
-
-        -0.5f, -0.5f, 0.5f,     0.0f,  0.0f,  1.0f,
-        0.5f,  -0.5f, 0.5f,     0.0f,  0.0f,  1.0f,
-        0.5f,  0.5f,  0.5f,     0.0f,  0.0f,  1.0f,
-        0.5f,  0.5f,  0.5f,     0.0f,  0.0f,  1.0f,
-        -0.5f, 0.5f,  0.5f,     0.0f,  0.0f,  1.0f,
-        -0.5f, -0.5f, 0.5f,     0.0f,  0.0f,  1.0f,
-
-        -0.5f, 0.5f,  0.5f,    -1.0f, 0.0f,  0.0f,
-        -0.5f, 0.5f,  -0.5f,   -1.0f, 0.0f,  0.0f,
-        -0.5f, -0.5f, -0.5f,   -1.0f, 0.0f,  0.0f,
-        -0.5f, -0.5f, -0.5f,   -1.0f, 0.0f,  0.0f,
-        -0.5f, -0.5f, 0.5f,    -1.0f, 0.0f,  0.0f,
-        -0.5f, 0.5f,  0.5f,    -1.0f, 0.0f,  0.0f,
-
-        0.5f,  0.5f,  0.5f,     1.0f,  0.0f,  0.0f,
-        0.5f,  0.5f,  -0.5f,    1.0f,  0.0f,  0.0f,
-        0.5f,  -0.5f, -0.5f,    1.0f,  0.0f,  0.0f,
-        0.5f,  -0.5f, -0.5f,    1.0f,  0.0f,  0.0f,
-        0.5f,  -0.5f, 0.5f,     1.0f,  0.0f,  0.0f,
-        0.5f,  0.5f,  0.5f,     1.0f,  0.0f,  0.0f,
-
-        -0.5f, -0.5f, -0.5f,    0.0f,  -1.0f, 0.0f,
-        0.5f,  -0.5f, -0.5f,    0.0f,  -1.0f, 0.0f,
-        0.5f,  -0.5f, 0.5f,     0.0f,  -1.0f, 0.0f,
-        0.5f,  -0.5f, 0.5f,     0.0f,  -1.0f, 0.0f,
-        -0.5f, -0.5f, 0.5f,     0.0f,  -1.0f, 0.0f,
-        -0.5f, -0.5f, -0.5f,    0.0f,  -1.0f, 0.0f,
-
-        -0.5f, 0.5f,  -0.5f,    0.0f,  1.0f,  0.0f,
-        0.5f,  0.5f,  -0.5f,    0.0f,  1.0f,  0.0f,
-        0.5f,  0.5f,  0.5f,     0.0f,  1.0f,  0.0f,
-        0.5f,  0.5f,  0.5f,     0.0f,  1.0f,  0.0f,
-        -0.5f, 0.5f,  0.5f,     0.0f,  1.0f,  0.0f,
-        -0.5f, 0.5f,  -0.5f,    0.0f,  1.0f,  0.0f
-    };
-
-    GLuint light_vertex_buffer, light_vertex_array_id;
-    glGenVertexArrays(1, &light_vertex_array_id);
-    glGenBuffers(1, &light_vertex_buffer);
-
-    glBindVertexArray(light_vertex_array_id);
-
-    glBindBuffer(GL_ARRAY_BUFFER, light_vertex_buffer);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(light_cube_vertexes),
-                 light_cube_vertexes, GL_STATIC_DRAW);
-    // vertex coords
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat),
-                          (GLvoid *)0);
-    glEnableVertexAttribArray(0);
-
-    // normal
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat),
-                          (GLvoid *)(3 * sizeof(GLfloat)));
-    glEnableVertexAttribArray(1);
-
-    glBindVertexArray(0);
-
-    ShaderProgram light_shader("shaders/light.vertex", "shaders/light.fragment");
+    Cube cube_of_shades;
+    cube_of_shades.translate({3.0f, 3.0f, 3.0f});
     // -------------------------------------------------------------------------
     Camera camera;
     Controller controller(&camera, window);
@@ -176,18 +108,10 @@ int main() {
         rainbow_pyramid.draw_shape(projection_view);
         // ---------------------------------------------------------------------
         // "cube of shade" draw
-        light_shader.use();
-        glUniform3f(light_shader.get_uniform_id("trans"), 3.0f, 3.0f, 3.0f);
-        glUniformMatrix4fv(light_shader.get_uniform_id("MVP"), 1, GL_FALSE,
-                           &MVP[0][0]);
-        glUniform3f(light_shader.get_uniform_id("light_color"),
-                    color_per_frame[0], color_per_frame[1], color_per_frame[2]);
-        auto center = rainbow_pyramid.get_center();
-        glUniform3f(light_shader.get_uniform_id("light_pos"),
-                    center[0], center[1], center[2]);
-        glBindVertexArray(light_vertex_array_id);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-        glBindVertexArray(0);
+        cube_of_shades.set_shader_model_matrix();
+        cube_of_shades.set_light_position(rainbow_pyramid.get_center());
+        cube_of_shades.set_light_color(color_per_frame);
+        cube_of_shades.draw(projection_view);
         // ---------------------------------------------------------------------
 
         glfwSwapBuffers(window);
