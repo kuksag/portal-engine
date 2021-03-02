b10ad1305bff0ef913656a5c63b80e2e920e5390
kuksag <georgiy.kuksa@gmail.com>
2021-03-02 21:25:08 +0300

    clang-format

--- src/primitives.cpp
+++ src/primitives.cpp
@@ -1,28 +1,34 @@
 #include "primitives.h"
 
 #include <glm/glm.hpp>
-#include <string>
 #include <memory>
+#include <string>
 
-Primitive::Primitive(const glm::vec3& position, const glm::vec3& color, const std::string& path) : Model(path, std::make_shared<ShaderProgram>("shaders/color.vertex", "shaders/color.fragment")) {
-	translate(position);
-	set_color(color);
+Primitive::Primitive(const glm::vec3& position, const glm::vec3& color,
+                     const std::string& path)
+    : Model(path, std::make_shared<ShaderProgram>("shaders/color.vertex",
+                                                  "shaders/color.fragment")) {
+    translate(position);
+    set_color(color);
 }
-
 
 void Primitive::set_color(const glm::vec3& color) {
-	this->color = color;
-	shader->use();
-	glUniform4f(shader->get_uniform_id("color"), color.x, color.y, color.z, 1);
+    this->color = color;
+    shader->use();
+    glUniform4f(shader->get_uniform_id("color"), color.x, color.y, color.z, 1);
 }
 
-glm::vec3 Primitive::get_color() const {
-	return color;
-}
+glm::vec3 Primitive::get_color() const { return color; }
 
-Sphere::Sphere(const glm::vec3& position, const glm::vec3& color) : Primitive(position, color, "res/models/primitives/sphere.obj") {}
-Cube::Cube(const glm::vec3& position, const glm::vec3& color) : Primitive(position, color, "res/models/primitives/cube.obj") {}
-Plane::Plane(const glm::vec3& position, const glm::vec3& color) : Primitive(position, color, "res/models/primitives/plane.obj") {}
-Cylinder::Cylinder(const glm::vec3& position, const glm::vec3& color) : Primitive(position, color, "res/models/primitives/cylinder.obj") {}
-Torus::Torus(const glm::vec3& position, const glm::vec3& color) : Primitive(position, color, "res/models/primitives/torus.obj") {}
-Cone::Cone(const glm::vec3& position, const glm::vec3& color) : Primitive(position, color, "res/models/primitives/cone.obj") {}
+Sphere::Sphere(const glm::vec3& position, const glm::vec3& color)
+    : Primitive(position, color, "res/models/primitives/sphere.obj") {}
+Cube::Cube(const glm::vec3& position, const glm::vec3& color)
+    : Primitive(position, color, "res/models/primitives/cube.obj") {}
+Plane::Plane(const glm::vec3& position, const glm::vec3& color)
+    : Primitive(position, color, "res/models/primitives/plane.obj") {}
+Cylinder::Cylinder(const glm::vec3& position, const glm::vec3& color)
+    : Primitive(position, color, "res/models/primitives/cylinder.obj") {}
+Torus::Torus(const glm::vec3& position, const glm::vec3& color)
+    : Primitive(position, color, "res/models/primitives/torus.obj") {}
+Cone::Cone(const glm::vec3& position, const glm::vec3& color)
+    : Primitive(position, color, "res/models/primitives/cone.obj") {}
